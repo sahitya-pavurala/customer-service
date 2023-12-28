259e30540ac3e1c1636e0463e9df61aee0419be1
sahitya-pavurala <sp313@nyu.edu>
2023-12-27 21:42:39 -0500

    add customers api and mock data

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+  "net/http"
   "github.com/gin-gonic/gin"
 )
+
+// customer struct to mock customer
+type customer struct {
+        ID     string  `json:"id"`
+        Name  string  `json:"name"`
+        AccountID     string  `json:"accountid"`
+}
+
+// customers slice to seed customer data.
+var customers = []customer{
+        {ID: "1", Name: "Luffy", AccountID: "123"},
+        {ID: "2", Name: "Zoro", AccountID: "345"},
+        {ID: "3", Name: "Sanji", AccountID: "678"},
+}
+
+
+func getCustomers(c *gin.Context) {
+        c.IndentedJSON(http.StatusOK, customers)
+}
+
 func main() {
   r := gin.Default()
+  r.GET("/customers", getCustomers)
 
   r.Run(":8080")
 }
