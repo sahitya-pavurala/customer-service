e3f18bcacfe783dd3ae5eec6a383bd0bed9ed264
sahitya-pavurala <sp313@nyu.edu>
2023-12-31 23:12:07 -0500

    add handler methods to separate files

--- main.go
+++ main.go
@@ -25,12 +25,15 @@ func main() {
 	// Public route for login
 	r.POST("/login", LoginHandler)
 
+	// Public route for monitoring
+	r.GET("/health", HealthHandler)
+
 	// Secured route for a customers endpoint
 	private := r.Group("/api")
 	private.Use(AuthMiddleware)
 	{
 		private.GET("/customers", GetAllCustomers)
-		private.GET("/customers/:id", GetCustomer)
+		private.GET("/customers/:id", GetCustomerUsingId)
 	}
 
 	r.Run(":8080")
