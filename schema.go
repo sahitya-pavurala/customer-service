e5460921fb66b9ef39bced70fad1d909c3f7ad5b
sahitya-pavurala <sp313@nyu.edu>
2024-01-01 22:22:51 -0500

    add more structure to customer and account

--- schema.go
+++ schema.go
@@ -1,7 +1,18 @@
 package main
 
-
 type User struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type Customer struct {
+	CustomerID      string    `json:"customer_id"`
+	Profile         string    `json:"profile"`
+	PhysicalAddress string    `json:"physical_address"`
+	Accounts        []Account `json:"accounts"`
+}
+
+type Account struct {
+	AccountID string `json:"account_id"`
+	Type      string `json:"account_type"`
 }
